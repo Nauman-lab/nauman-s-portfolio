56b87ff94f9db54e004221a57c35c35f8dbe8a2e
Nauman-lab <naumanrasheed269@gmail.com>
2024-04-03 23:57:55 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hi Ashiqo</h1>
+      <button className="btn btn-secondary">Two</button>
+    </>
+  );
+}
